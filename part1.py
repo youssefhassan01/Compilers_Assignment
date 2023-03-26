c769372019d33487e5623049e705207355514888
youssefhassan01 <youssef.hassan01@eng-st.cu.edu.eg>
2023-03-26 17:45:13 +0200

    Grouping functioonality

--- part1.py
+++ part1.py
@@ -1,41 +1,53 @@
-Regex = input("Enter Regex:")
+import re
 
-size=len(Regex)
+class state:
+    isTerminalState = None
+    inputCharacters = None
+    def __init__(self,tState=False,inChar={}):
+        self.isTerminalState = tState
+        self.inputCharacters=inChar
 
-squareCounter = 0
-bracketCounter = 0
-curlyCounter = 0
-invalid = 0
-for i in range(size):
-    if((Regex[i] == "*") or (Regex[i] == "?") or (Regex[i] == "+")):
-        continue
-    elif(Regex[i] == "|" or Regex[i] == "^" or Regex[i] == "$"):
-        continue
-    elif(Regex[i] == "["):
-        squareCounter+=1
-    elif(Regex[i] == "{"):
-        curlyCounter+=1
-    elif(Regex[i] == "("):
-        bracketCounter+=1
-    elif(Regex[i] == "]"):
-        squareCounter-=1
-    elif(Regex[i] == "}"):
-        curlyCounter-=1
-    elif(Regex[i] == ")"):
-        bracketCounter-=1
-    elif(Regex[i].isalnum()):
-        continue
-    else:
-        invalid=1
-        break
+def makeNFA(regexInput,NFA):
+    if(regexInput == ""):
+        return NFA
+    c=regexInput[0]
+    # Grouping
+    if(c == "("):
+        counter=1
+        index=0
+        print("indexed is")
+        print(regexInput[index])
+        while (counter != 0):
+            if(regexInput[index] == "("):
+                counter+=1
+            elif(regexInput[index] == ")"):
+                counter-=1
+            index+=1
+        print(regexInput[1:index])
+        NFA=makeNFA(regexInput[1:index],NFA)
 
-if( not (squareCounter == 0 and curlyCounter == 0 and bracketCounter == 0 )):
-    invalid = 1
-    
-if(invalid == 1):
-    print("Error Invalid Regex")
-else:
-    for i in range(size):
-        
- 
-        
+# Regex = input("Enter Regex:")
+
+# size=len(Regex)
+
+
+# try:     
+#     re.compile(Regex)   
+# except re.error:     
+#     print("Non valid regex pattern")     
+#     exit()
+
+# 1. Required scope for the input regular expressions:
+#   - Alternation a|b 
+#   - Concatenation ab
+#   - 1 or more a*
+#   - 0 or more a+
+#   - Optional a?
+#   - Character/number classes [abc] or [345]
+#   - Ranges [a-c] or [0-5]
+#   - Brackets for grouping ()
+# 2. You can use any third-party library to check the validity of the input regular expression.
+# 3. You can use any third-party library to help you draw a diagram of your FSM and deal with it as you like, as long as the JSON output file is as expected and stated in the assignment document.
+
+NFA = {}
+NFA = makeNFA("((ab|d)|c)",NFA)
