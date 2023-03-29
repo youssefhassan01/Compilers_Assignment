19146c91e77541680683b81f1784b1152c44479b
Joseph Ameer Aziz <joseph.ameer21@gmail.com>
2023-03-29 07:24:35 +0200

    ClassRange logic and concatenation

--- part1.py
+++ part1.py
@@ -51,6 +51,13 @@ class superstate:
 
 recursioncounter = 0
 
+# [expressions, operations]
+# expressions = string | operations(expression)
+# operations = functions(expressions)
+superstate_stack = []
+AllStates = []
+
+
 # gets string in form of something "(something)"
 
 
@@ -72,19 +79,43 @@ def getIndexEndingBrack(stringInput, bracket):
     return index-1
 
 
-def ClassRangeLogic(regexInput):
-    pass
+def pipeLogic():
+    ss2 = superstate_stack.pop()
+    ss1 = superstate_stack.pop()
+
+    newStartState = state()
+    newStartState.appendToEpsilon(ss1.startState)
+    newStartState.appendToEpsilon(ss2.startState)
+
+    newEndState = state()
+    ss1.endState.appendToEpsilon(newEndState)
+    ss2.endState.appendToEpsilon(newEndState)
+    ss1.endState.stateDict["isTerminalState"] = False
+    ss2.endState.stateDict["isTerminalState"] = False
+
+    newSuperState = superstate(newStartState, newEndState)
+    superstate_stack.append(newSuperState)
 
 
-# [expressions, operations]
+def RangeLogic(regexInput, notFirstRange):
+    print("Handling range", regexInput)
 
-# expressions = string | operations(expression)
-# operations = functions(expressions)
-superstate_stack = []
-AllStates = []
+    firstState = state()
+    secondState = state()
+    firstState.addTransition(secondState, regexInput)
+    # print("namaywa", firstState.name)
+    # print(NFA)
+    Superstate = superstate(firstState, secondState)
+    superstate_stack.append(Superstate)
+
+    # insert pipelogic here
+    if (notFirstRange):
+        pipeLogic()
 
 
-def concatLogic():
+def concatLogic(previouscharacter):
+    if (not previouscharacter.isalnum() and not previouscharacter in ')]+*?'):
+        return
     global superstate_stack
 
     if (len(superstate_stack) > 1):
@@ -107,7 +138,44 @@ def concatLogic():
         superstate_stack.append(newSuperState)
 
 
-# makeNFAcurrIndex = 0
+def ClassRangeLogic(regexInput, previouscharacter):
+
+    # find - (the dash) and remove it together with surrounding characters
+    notFirstRange = False
+    # bootstrap the while loop by setting it to something other than -1
+    dashIndex = 1
+    # remove all ranges
+    while (dashIndex != -1):
+        dashIndex = regexInput.find('-')
+        if (dashIndex != -1):
+            RangeLogic(regexInput[dashIndex-1:dashIndex+2], notFirstRange)
+            # print(regexInput[dashIndex-1:dashIndex+2])
+            # regexInput.replace(regexInput[dashIndex-1:dashIndex+2], '')
+            regexInput = regexInput[:dashIndex-1] + regexInput[dashIndex+2:]
+            notFirstRange = True
+            print('new replaced regex input is ', regexInput)
+    # what remains is the class logic
+    if (len(regexInput) == 0):
+        return
+    counter = 0
+    for i in range(1, len(regexInput)):  # from second position to before last character
+        print('lol', i)
+        regexInput = regexInput[:i+counter] + '|' + regexInput[i+counter:]
+        counter += 1
+
+    # if (notFirstRange):
+    #     regexInput = '|'+regexInput
+    print('class regexinput is ', regexInput)
+    makeNFA(regexInput)
+
+    # or the classes with the ranges
+    if (notFirstRange):
+        pipeLogic()
+    # print(previous_c)
+    # if (previouscharacter.isalnum() or previouscharacter in ')]+*?'):
+    #     concatLogic()
+    # # one more call here because it returns outside
+    # concatLogic(previouscharacter)
 
 
 def makeNFA(regexInput, contextindex=0):
@@ -135,33 +203,61 @@ def makeNFA(regexInput, contextindex=0):
         # mfee4 -1, excluded by default
         newregexInput = regexInput[contextindex+1:contextindex+index]
 
-        print("new input is ", newregexInput)
-        # parese new expression
+        # print("new input is ", newregexInput)
+        # parese new expression in the brackets
         makeNFA(newregexInput)
+        # concatenate the brackets states
+        if (contextindex != 0):
+            previous_c = regexInput[contextindex-1]
+            concatLogic(previous_c)
         # parse from the beginning of the ending bracket
         # print(regexInput[index:])
         makeNFA(regexInput[index+1:])  # skip the bracket
         return
+
+    # range or group
+    if (c == '['):
+        index = getIndexEndingBrack(regexInput[contextindex:], '[')
+        # print(regexInput[1:index])
+        recursioncounter += 1
+        newregexInput = regexInput[contextindex+1:contextindex+index]
+        previouscharacter = regexInput[contextindex -
+                                       1] if (contextindex > 0) else ''
+        print('previouscharacter is ', previouscharacter)
+        # print(newregexInput)
+        ClassRangeLogic(newregexInput, previouscharacter)
+        if (contextindex != 0):
+            previous_c = regexInput[contextindex-1]
+            concatLogic(previous_c)
+        makeNFA(regexInput[index+1:])  # skip the bracket
+        return
+
     if (c == "|"):
         # call next expression first
         makeNFA(regexInput, contextindex+1)
         # handle pipe logic
         print("found | at contextindex= ", contextindex)
-        ss2 = superstate_stack.pop()
-        ss1 = superstate_stack.pop()
+        pipeLogic()
+        # CHECKKK
+        # not sure 5ales
+        if (contextindex != 0):
+            previous_c = regexInput[contextindex-1]
+            concatLogic(previous_c)
+        # ss2 = superstate_stack.pop()
+        # ss1 = superstate_stack.pop()
 
-        newStartState = state()
-        newStartState.appendToEpsilon(ss1.startState)
-        newStartState.appendToEpsilon(ss2.startState)
+        # newStartState = state()
+        # newStartState.appendToEpsilon(ss1.startState)
+        # newStartState.appendToEpsilon(ss2.startState)
 
-        newEndState = state()
-        ss1.endState.appendToEpsilon(newEndState)
-        ss2.endState.appendToEpsilon(newEndState)
-        ss1.endState.stateDict["isTerminalState"] = False
-        ss2.endState.stateDict["isTerminalState"] = False
+        # newEndState = state()
+        # ss1.endState.appendToEpsilon(newEndState)
+        # ss2.endState.appendToEpsilon(newEndState)
+        # ss1.endState.stateDict["isTerminalState"] = False
+        # ss2.endState.stateDict["isTerminalState"] = False
 
-        newSuperState = superstate(newStartState, newEndState)
-        superstate_stack.append(newSuperState)
+        # newSuperState = superstate(newStartState, newEndState)
+        # superstate_stack.append(newSuperState)
         # concatLogic()
 
     if (c == '*'):
@@ -181,8 +277,17 @@ def makeNFA(regexInput, contextindex=0):
         newSuperState = superstate(newStartState, newEndState)
         superstate_stack.append(newSuperState)
 
+        # if (contextindex != 0):
+        #     previous_c = regexInput[contextindex-1]
+        #     # print(previous_c)
+        #     # if (previous_c.isalnum() or previous_c in ')]+*?'):
+        #     #     concatLogic()
+        #     concatLogic(previous_c)
         # makeNFA(regexInput[1:])
         # continue parsing
+        if (contextindex != 0):
+            previous_c = regexInput[contextindex-1]
+            concatLogic(previous_c)
         makeNFA(regexInput, contextindex+1)
 
     if (c == '+'):
@@ -201,7 +306,10 @@ def makeNFA(regexInput, contextindex=0):
         newSuperState = superstate(newStartState, newEndState)
         superstate_stack.append(newSuperState)
 
-        # makeNFA(regexInput[1:])
+        if (contextindex != 0):
+            previous_c = regexInput[contextindex-1]
+            concatLogic(previous_c)
+
         # continue parsing
         makeNFA(regexInput, contextindex+1)
 
@@ -212,18 +320,12 @@ def makeNFA(regexInput, contextindex=0):
         # ss.startState.stateDict["epsilon"].append(ss.endState.name)
         ss.startState.appendToEpsilon(ss.endState)
         superstate_stack.append(ss)
+        if (contextindex != 0):
+            previous_c = regexInput[contextindex-1]
+            concatLogic(previous_c)
+
         makeNFA(regexInput, contextindex+1)
 
-    # range or group
-    if (c == '['):
-        index = getIndexEndingBrack(regexInput, '[')
-        # print(regexInput[1:index])
-        recursioncounter += 1
-        newregexInput = regexInput[1:index-1]
-        # print(newregexInput)
-        ClassRangeLogic(newregexInput)
-        makeNFA(regexInput[index:])
-        return
     # makeNFA(regexInput[], NFA)
     if (c.isalnum()):  # do we need to add other characters here ?
         print("found letter/num", c)
@@ -237,28 +339,42 @@ def makeNFA(regexInput, contextindex=0):
         superstate_stack.append(Superstate)
         if (contextindex != 0):
             previous_c = regexInput[contextindex-1]
-            # print(previous_c)
-            if (previous_c.isalnum()):
-                concatLogic()
+            concatLogic(previous_c)
         makeNFA(regexInput, contextindex+1)
+    # concatenate after all
+    # if (contextindex != 0):
+    #     previous_c = regexInput[contextindex-1]
+    #     # print(previous_c)
+    #     # if (previous_c.isalnum() or previous_c in ')]+*?'):
+    #     #     concatLogic()
+    #     concatLogic(previous_c)
 
 # how does our function handle abc? (da m3nah concatenation)
 # abc
 # (abc|[a-z])
 
 
-# alpha.(   && alpha .[
-# ).alpha|(
-# myind = getIndexEndingBrack("(cd)", "(")
-# print("myind is ", myind)
-
 NFA = {}
-# NFA = makeNFA("((ab|d)|c)")
+# Regex = "((a)(b))|((c)(d))"
+# try:
+#     re.compile(Regex)
+# except re.error:
+#     print("Non valid regex pattern")
+#     exit()
+# makeNFA(Regex)
+
+# ERROR CASES
+makeNFA("x?[0-9]+")  # gives error hena
+
+# makeNFA("((ab|d)|c)")
+# makeNFA("[a-cb]*")
+# makeNFA("[abc]")
 # makeNFA("a?")
 # makeNFA("(((a)(b)|(d))|(c))")
 # makeNFA("abc")  # works
 # makeNFA("ab|cd")
-# makeNFA("(ab)|(cd)")  # error
+# makeNFA("((a)(b)|((c)(d))")
+# makeNFA("(ab)|(cd)")  # works
 # makeNFA("(abc|3)")
 # print(superstate_stack[0].startState)
 # print(superstate_stack[0].endState)
@@ -278,11 +394,6 @@ for s in AllStates:
 
 # Regex = '[(hi)-z]'
 
-# try:
-#     re.compile(Regex)
-# except re.error:
-#     print("Non valid regex pattern")
-#     exit()
 
 # 1. Required scope for the input regular expressions:
 #   - Alternation a|b
