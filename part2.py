b8f8528a37d00dfd211f8cc4747a854f979f660b
youssefhassan01 <youssef.hassan01@eng-st.cu.edu.eg>
2023-04-06 15:10:33 +0200

    added epsilon closure

--- part2.py
+++ part2.py
@@ -2,21 +2,44 @@ import part1 as thomNFA
 from queue import Queue
 from utils import utils
 
-def epsilonClosure(state,AllStates):
-    newState = set()
+def checkUnique(list,elem):
+    if(list.count(elem) >= 1):
+        return False
+    else:
+        return True
+
+
+# gets epsilon clsoure of passed state using allStates after conversion to json  
+def epsilonClosure(state,allStates,isStart=True):
+    newState = list()
     findEpsilon = Queue(maxsize=0)
-    findEpsilon.put(state)
+    # Split passed state to key and value and add to Queue
+    key = list(state.keys())
+    key = key[0]
+    value = list(state.values())
+    value = value[0]
+    findEpsilon.put(value)
+    if isStart == True:
+        newState.append(state)
     while not findEpsilon.empty():
+        # walk through each state in epsilon of current state if any epsilon list exists
         x = findEpsilon.get()
-        if "epsilon" in x.stateDict:
-            for i in x.stateDict['epsilon']:
-                y = AllStates.get("")
-                findEpsilon.put(i)
-        newState.add(x)
+        if "epsilon" in x:
+            for i in x['epsilon']:
+                # since there can be no sets of dictitionary i made a list with a check to make sure it is unique and
+                # and not keep returning it to queue or state 
+                if(checkUnique(newState,{i:allStates.get(i)})):
+                    findEpsilon.put(allStates.get(i))
+                    newState.append({i:allStates.get(i)})
+    #returns all states in this epsilon closure
     return newState
 
+#TODO: function that accepts all alphabets and check for their epsilon closure
+#TODO: function that assembles DFA states !!! don't do this alone ya joe
 
-regex="a|b"
+#NOTE: when reading output of epsilon closure please read carefully as it outputs key and state in a dictionory  
+
+regex="(a|b)"
 adam=thomNFA.makeNFA(regex)
 
 alphabet = []
@@ -24,20 +47,14 @@ alphabet = []
 allStates = utils.convertAllstates(thomNFA.AllStates)
 utils.drawNFA(allStates,"NFA")
 
-for s,k in allStates:
-    print(s)
+# rudimentary system to get alphabet of regex
+# for c in regex:
+#     if(c.isalnum()):
+#         alphabet.append(c)
 
-for c in regex:
-    if(c.isalnum()):
-        alphabet.append(c)
+startingState = allStates.get("startingState")
 
-startingStates = []
-for s,k in allStates.items():
-    if("isStartState" in k.stateDict):
-        if(s.stateDict['isStartState'] == True):
-            startingStates.append(s)
-
-initState=epsilonClosure(startingStates)
+initState=epsilonClosure({startingState:allStates.get(startingState)},allStates)
 
 print(initState)
 
